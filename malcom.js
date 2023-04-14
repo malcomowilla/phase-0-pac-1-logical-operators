14f4cef49fa5d54736de4c5fdeac7ae3466fca35
malcomowilla <owillamalcom@gmail.com>
2023-04-14 13:08:56 +0300

    solved

---
+++ malcom.js
@@ -0,0 +1,36 @@
+// const truthyValue = 'This value is truthy.'
+// const falseyValue = 0;
+// console.log(!!truthyValue)
+// console.log(!!falseyValue)
+
+//What should each of the following expressions return? 
+//Once you have an answer, copy the expression in question into the console and hit enter to see if you're right!
+
+// Practice with AND
+const shinra = 0 && false;
+const adollBurst = 0 && true;
+const saitama = true && !1;
+const adollaLink = true && NaN;
+const hero = !0 && "This is a string"
+const arigato = !0 && !!"";
+console.log(adollBurst)
+console.log(shinra)
+console.log(saitama)
+console.log(adollaLink)
+console.log(hero)
+console.log(arigato)
+// Practice with OR
+
+const gozaimas = 0 || false;
+const simimasen = true || false;
+const konichiwa = true || 1;
+const baka = !true || !false;
+const pendekhooPuta = !1 || !0
+console.log(gozaimas)
+console.log(simimasen)
+console.log(konichiwa)
+console.log(baka)
+console.log(pendekhooPuta)
+
+
+
